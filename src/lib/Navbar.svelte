1bf46fba58ed1af1ff500b7cd1ffeae11d160ddd
Dominik Bartsch <dominik.bartsch@gmx.net>
2023-06-10 22:04:06 +0200

    Add navbar
    Add title for main page.

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let brand;
+</script>
+
+<nav>
+    <a href="/">{brand}</a>
+    <a href="/#about">About</a>
+    <a href="/#projects">Projects</a>
+    <a href="https://github.com/{brand}">GitHub</a>
+</nav>
+
+<style>
+    nav {
+        background: greenyellow;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
     import 'normalize.css';
     import './styles.css';
 
+    import Navbar from "../lib/Navbar.svelte";
+
+    let brand = "domfoo";
 </script>
 
+<Navbar {brand}></Navbar>
 <slot></slot>
